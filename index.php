7b9b14c070d25f73571e0e2ef2bdb4f9ade226d6
mrseo <mr.seo@mail.ru>
2013-03-30 13:06:40 +0300

    YandexPars

---
+++ index.php
@@ -0,0 +1,17 @@
+﻿<?php
+require_once('simple_html_dom.php'); //здесь подключаю библиотеку simplhtmldom
+$html = file_get_html('http://www.yandex.ru/'); // парсим главную яндекса
+$text=$html->find('.b-news__tabs,.b-news__news'); //при помощи метода find отбираем текст и данных классов
+$e=implode(" ",$text); //объединяем в строку
+file_put_contents("parse.txt","$e"); //сохраняем в файл parse.txt
+echo $e; //выводим полученные блоки
+$image=$html->find('img[width]'); //парсим картинки
+echo "Три картинки с главной яндекса:<br>";
+$k=0;
+foreach ($image as $img) //поочередно выводим каждую картинку одна под другой
+{
+	$k++;
+	echo "Картинка яндекса $k : <br>";
+	echo "$img<br>";
+}
+?>
